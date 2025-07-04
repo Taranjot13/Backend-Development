c63d5cdb0c793fab247698aec79f5121a21d543b
Taranjot Singh <65taranjot@gmail.com>
2025-07-04 15:11:37 +0530

    done work

---
+++ class/lec 2/ESModule/index.js
@@ -0,0 +1,14 @@
+export function add(a, b) {
+    return a + b;
+}   
+export function sub(a, b) {
+    return a - b;
+}  
+export function mult(a, b) {
+    return a * b;
+}
+
+
+export default mult;
+// export default add;
+// export default sub;
---
+++ class/lec 2/index.js
@@ -0,0 +1,15 @@
+// MODULE
+// This module exports a function that returns a greeting message.
+// reusable piece of code
+
+function add(a,b){
+    return a + b;
+}
+function sub(a,b){
+    return a - b;
+}
+
+module.exports.add = add;
+module.exports.sub = sub;   
+
+
