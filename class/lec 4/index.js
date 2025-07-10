275841b668b53cd799649ddec2251c426c4196ec
Taranjot Singh <65taranjot@gmail.com>
2025-07-10 15:47:45 +0530

    done work

--- class/lec 4/index.js
+++ class/lec 4/index.js
@@ -12,3 +12,13 @@ buyproduct("Iphone 16",function(){
 function buyproduct(product_name,cb){
     let isproduct=null;
 }
+buyproduct("Iphone 16",function(){
+    console.log("product is purchased");
+    if(err) return console.log(err)
+        console.log(amount);
+    deductAmount(amount,function(err,amount){
+        if(err) return console.log(err);
+        console.log(message);
+        console.log(amount);
+    });
+});
