ddd62eb3d630d9c40da00634e6635e838b4b832a
Taranjot Singh <65taranjot@gmail.com>
2025-11-25 10:16:23 +0530

    done

--- class/lec 39/index.js
+++ class/lec 39/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const User = require('./model/user');
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send("ok")
+});
+
 mongoose.connect('mongodb://127.0.0.1:27017/test') 
     .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
 app.listen(7687, () => {
-    console.log('Server started');
+    console.log('Server started on port 7687');
 });
